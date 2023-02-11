173231c7a9f0cd92f44d136e76d6d573d0a389e5
Manny Cortez <mannycortezstudios@gmail.com>
2023-02-11 10:46:17 -0500

    Added NavBar and Footer components - base

---
+++ components/NavBar.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NavBar = () => (
+  <div style={{ color: 'red' }}>NavBarhello</div>
+);
+
+export default NavBar;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,17 @@
+/* eslint-disable import/no-unresolved */
 /* eslint-disable react/jsx-props-no-spreading */
 import { ThemeProvider } from 'next-themes';
+import NavBar from 'components/NavBar';
+import Footer from 'components/Footer';
 
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }) => (
   <ThemeProvider attribute="class">
     <div>
-      <h1>NAVBAR</h1>
+      <NavBar />
       <Component {...pageProps} />
-      <h1>FOOTER</h1>
+      <Footer />
     </div>
   </ThemeProvider>
 );
